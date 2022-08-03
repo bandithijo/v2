b3b12ec651be24322084fbd2f978fd3f7bf4540e
bandithijo <bandithijo@users.noreply.github.com>
2022-08-03 02:12:02 +0000

    jekyll build from Action a56cab7f26f4529ad3867e1c8f22acab50dcc940

---
+++ assets/js/headings-permalink.js
@@ -0,0 +1,9 @@
+var headings = document.querySelectorAll("h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]");
+
+for (var i = 0; i < headings.length; i++) {
+  headings[i].innerHTML =
+    '<a style="text-decoration:none;color:#ba3925;" href="#' + headings[i].id + '">' +
+    headings[i].innerText +
+    '</a>'
+  ;
+}
